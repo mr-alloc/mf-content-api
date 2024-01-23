1ffc3d880afa0cf811e764efd142fc6294e7fa52
mr-alloc <mr.allocater@gmail.com>
2024-01-23 16:44:39 +0900

    project setting
    프로젝트 세팅

---
+++ mf-content-api/src/main/java/com/cofixer/mf/mfcontentapi/MfContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.cofixer.mf.mfcontentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfContentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfContentApiApplication.class, args);
+	}
+
+}
