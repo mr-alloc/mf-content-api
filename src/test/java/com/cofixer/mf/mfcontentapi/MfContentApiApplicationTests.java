96d6ddec1183305883e5356bd302a018908979b3
mr-alloc <mr.allocater@gmail.com>
2024-01-23 16:45:49 +0900

    프로젝트 세팅

---
+++ src/test/java/com/cofixer/mf/mfcontentapi/MfContentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofixer.mf.mfcontentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfContentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
