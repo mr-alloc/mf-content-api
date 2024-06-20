a6145c1c2d1da804b61672cf907bd44c4eca5e2f
mr-alloc <mr.allocater@gmail.com>
2024-06-20 18:08:13 +0900

    MissionController 문서화 내용 추가

--- src/main/java/com/cofixer/mf/mfcontentapi/controller/MissionController.java
+++ src/main/java/com/cofixer/mf/mfcontentapi/controller/MissionController.java
@@ -8,6 +8,8 @@ import com.cofixer.mf.mfcontentapi.dto.req.CreateMissionReq;
 import com.cofixer.mf.mfcontentapi.dto.res.*;
 import com.cofixer.mf.mfcontentapi.service.AuthorizedService;
 import com.cofixer.mf.mfcontentapi.service.MissionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @AccountAuth(AccountRoleType.MEMBER)
+@Tag(name = "/mission: 미션")
 @RequestMapping("/v1/mission")
 public class MissionController {
 
     private final MissionService missionService;
 
 
+    @Operation(summary = "/create: 생성")
     @PostMapping
     public ResponseEntity<CreateMissionRes> createMission(@RequestBody CreateMissionReq req) {
         AuthorizedMember authorizedMember = AuthorizedService.getMember();
@@ -33,6 +37,7 @@ public class MissionController {
                 .body(CreateMissionRes.of(missions));
     }
 
+    @Operation(summary = "/calendar: 이번달 스케쥴 조회")
     @GetMapping("/calendar")
     public ResponseEntity<GetMemberCalendarRes> getMemberCalendar(
             @RequestParam("startAt") Long startAt,
@@ -43,6 +48,7 @@ public class MissionController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "/{missionId}: 상세 조회")
     @GetMapping("/{missionId}")
     public ResponseEntity<GetMissionDetailRes> getMissionDetail(@PathVariable("missionId") Long missionId) {
         AuthorizedMember authorizedMember = AuthorizedService.getMember();
@@ -51,6 +57,7 @@ public class MissionController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "/{missionId}: 변경")
     @PutMapping("/{missionId}")
     public ResponseEntity<ChangeMissionRes> changeMission(
             @PathVariable("missionId") Long missionId,
@@ -61,6 +68,7 @@ public class MissionController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "/{missionId}: 삭제")
     @DeleteMapping("/{missionId}")
     public ResponseEntity<DeleteMissionRes> deleteMission(@PathVariable("missionId") Long missionId) {
         AuthorizedMember authorizedMember = AuthorizedService.getMember();
